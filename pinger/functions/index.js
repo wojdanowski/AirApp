9c9c82e8dbceb30e7fe3a6643889107a6ce474c9
wojdanowski <s.wojdanowski@gmail.com>
2020-04-26 22:04:52 +0200

    Pinger app.listen removed

--- pinger/functions/index.js
+++ pinger/functions/index.js
@@ -6,18 +6,18 @@ const app = express();
 const port = 3000;
 const wakeUpIntervalMin = 28;
 
-app.listen(port, function () {
-	console.log(`Listening to Port ${port}`);
-});
+// app.listen(port, function () {
+// 	console.log(`Listening to Port ${port}`);
+// });
 
-app.get('/', (req, res) => {
-	res.send('Pinger is running');
-});
+// app.get('/', (req, res) => {
+// 	res.send('Pinger is running');
+// });
 
 const sendCall = () => {
 	console.log('Sending wake up call');
 	https
-		.get('https://air-data-api.herokuapp.com', (resp) => {
+		.get('https://air-data-api.herokuapp.com/api/', (resp) => {
 			let data = '';
 
 			// A chunk of data has been recieved.
