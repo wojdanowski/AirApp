cf80a717085b103dae9855f9307d43c7e4195e5b
pgarr <garlej.p@gmail.com>
2020-04-27 20:51:53 +0200

    subscriptionCOntroller update and delete fix

--- api/src/controllers/subscriptionController.js
+++ api/src/controllers/subscriptionController.js
@@ -3,7 +3,14 @@ const Subscription = require('../models/subscriptionModel');
 exports.new = async (req, res) => {
   // TODO: walidacja danych
   try {
-    const newSubscription = await Subscription.create(req.body);
+    const newSubscription = await Subscription.create({
+      email: req.body.email,
+      location: {
+        type: 'Point',
+        coordinates: [req.body.lon, req.body.lat],
+      },
+      hours: req.body.hours,
+    });
 
     res.status(201).json({
       status: 'success',
@@ -43,10 +50,17 @@ exports.update = async (req, res) => {
   // TODO: wyszukiwanie po tokenie jwt, a nie id
 
   try {
-    const subscription = await Subscription.findByIdandUpdate(
+    const subscription = await Subscription.findByIdAndUpdate(
       req.params.subscription_id,
-      req.body,
       {
+        location: {
+          type: 'Point',
+          coordinates: [req.body.lon, req.body.lat],
+        },
+        hours: req.body.hours,
+      },
+      {
+        new: true,
         runValidators: true,
       }
     );
@@ -54,12 +68,12 @@ exports.update = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour,
+        subscription,
       },
     });
   } catch (err) {
     res.status(404).json({
-      //TODO: no 404 to średnio, mogą być neipoprawne dane
+      //TODO: no 404 to średnio, mogą być niepoprawne dane
       status: 'fail',
       message: err,
     });
@@ -71,13 +85,15 @@ exports.delete = async (req, res) => {
   // TODO: wyszukiwanie po tokenie jwt, a nie id
 
   try {
-    await Subscription.findByIdandDelete(req.params.id);
+    await Subscription.findByIdAndDelete(req.params.subscription_id);
 
+    // FIXME: kod 204 = no content, dlatego json nie ma sensu
     res.status(204).json({
       status: 'success',
       data: null,
     });
   } catch (err) {
+    // FIXME: nawet jak nic nie znajdzie, to nie rzuci rejecta. Niby ok, miało nie być i nie ma, ale powinien być ten 404
     res.status(404).json({
       status: 'fail',
       message: err,
