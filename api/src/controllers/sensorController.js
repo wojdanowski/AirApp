2dd7c726f14724504082c5e467fc74835009c32c
pgarr <garlej.p@gmail.com>
2020-05-10 11:46:26 +0200

    sensors list without __v

--- api/src/controllers/sensorController.js
+++ api/src/controllers/sensorController.js
@@ -1,8 +1,15 @@
 const Sensor = require('../models/sensorModel');
+const DbQueryFeatures = require('../utils/dbQueryFeatures');
 
 exports.stationSensorsData = async (req, res) => {
   try {
-    const sensors = await Sensor.find({ station: req.params.stationId });
+    const features = new DbQueryFeatures(
+      Sensor.find({ station: req.params.stationId }),
+      req.query
+    ).limitFields();
+    const sensors = await features.query;
+
+    // const sensors = await Sensor.find({ station: req.params.stationId });
     if (sensors.length === 0) {
       res.status(404).json({
         status: 'fail',
