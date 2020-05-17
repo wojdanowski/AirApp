30ffc987c463974bde1350d369b2953816b8d619
pgarr <garlej.p@gmail.com>
2020-05-17 14:43:48 +0200

    tests for all endpoints

--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -52,7 +52,19 @@ describe('Stations endpoints', () => {
     expect(res.body.status).toEqual('success');
     expect(res.body).toHaveProperty('data');
 
-    expect(res.body.data.station.name).toEqual('Wrocław - Bartnicza');
+    const { station } = res.body.data;
+
+    expect(station.name).toEqual('Wrocław - Bartnicza');
+
+    expect(station).toHaveProperty('stIndex');
+    expect(station).toHaveProperty('_id');
+    expect(station).toHaveProperty('location');
+    expect(station).toHaveProperty('mIndexes');
+    expect(station).toHaveProperty('source');
+
+    expect(station).toHaveProperty('distance');
+    expect(station.distance).toBeGreaterThan(0.7);
+    expect(station.distance).toBeLessThan(0.8);
   });
 
   it('Should return nearest station Wroclaw - Korzeniowskiego', async () => {
@@ -65,7 +77,19 @@ describe('Stations endpoints', () => {
     expect(res.body.status).toEqual('success');
     expect(res.body).toHaveProperty('data');
 
-    expect(res.body.data.station.name).toEqual('Wrocław - Korzeniowskiego');
+    const { station } = res.body.data;
+
+    expect(station.name).toEqual('Wrocław - Korzeniowskiego');
+
+    expect(station).toHaveProperty('stIndex');
+    expect(station).toHaveProperty('_id');
+    expect(station).toHaveProperty('location');
+    expect(station).toHaveProperty('mIndexes');
+    expect(station).toHaveProperty('source');
+
+    expect(station).toHaveProperty('distance');
+    expect(station.distance).toBeGreaterThan(2.9);
+    expect(station.distance).toBeLessThan(3);
   });
 
   it('Should return nearest station Legnica - Rzeczypospolitej', async () => {
@@ -78,7 +102,65 @@ describe('Stations endpoints', () => {
     expect(res.body.status).toEqual('success');
     expect(res.body).toHaveProperty('data');
 
-    expect(res.body.data.station.name).toEqual('Legnica - Rzeczypospolitej');
+    const { station } = res.body.data;
+
+    expect(station.name).toEqual('Legnica - Rzeczypospolitej');
+
+    expect(station).toHaveProperty('stIndex');
+    expect(station).toHaveProperty('_id');
+    expect(station).toHaveProperty('location');
+    expect(station).toHaveProperty('mIndexes');
+    expect(station).toHaveProperty('source');
+
+    expect(station).toHaveProperty('distance');
+    expect(station.distance).toBeGreaterThan(0.8);
+    expect(station.distance).toBeLessThan(0.9);
+  });
+
+  it('Should return 422 for nearest station without lat/lon', async () => {
+    const res = await request(app).get('/api/nearestAirIndex');
+    expect(res.statusCode).toEqual(422);
+    expect(res.body.status).toEqual('fail');
+    expect(res.body).toHaveProperty('message');
+  });
+});
+
+describe('Sensors endpoints', () => {
+  it('Should return sensors for correct station', async () => {
+    const stationId = '5eb3f488bc7e8f9cc8d7bb0b';
+    const res = await request(app).get(`/api/stations/sensors/${stationId}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.status).toEqual('success');
+    expect(res.body).toHaveProperty('data');
+
+    const sensors = res.body.data;
+
+    expect(sensors.length).toBeGreaterThan(0);
+
+    expect(sensors[0]).toHaveProperty('key');
+    expect(sensors[0]).toHaveProperty('_id');
+    expect(sensors[0]).toHaveProperty('station');
+    expect(sensors[0]).toHaveProperty('values');
+    expect(sensors[0].values.length).toBeGreaterThan(0);
+  });
+
+  it('Should return 404 for non existing station', async () => {
+    const stationId = '5eb443e8bbb79f259d22d6c2';
+    const res = await request(app).get(`/api/stations/sensors/${stationId}`);
+
+    expect(res.statusCode).toEqual(404);
+    expect(res.body.status).toEqual('fail');
+    expect(res.body).toHaveProperty('message');
+  });
+
+  it('Should return 400 for invalid station id', async () => {
+    const stationId = 'xx';
+    const res = await request(app).get(`/api/stations/sensors/${stationId}`);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.status).toEqual('fail');
+    expect(res.body).toHaveProperty('message');
   });
 });
 
