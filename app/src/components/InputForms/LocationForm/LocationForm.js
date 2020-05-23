f3682d5a3169426cd4318b9a0efbc0be0c54fd80
wojdanowski <s.wojdanowski@gmail.com>
2020-05-23 14:35:31 +0200

    hide keayboard after form submit

--- app/src/components/InputForms/LocationForm/LocationForm.js
+++ app/src/components/InputForms/LocationForm/LocationForm.js
@@ -5,9 +5,12 @@ import GeoIcon from './../../UI/GeoIcon/GeoIcon';
 import PlaceSuggestion from './PlaceSuggestion/PlaceSuggestion';
 
 class LocationForm extends Component {
-	state = {
-		typedLocation: '',
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			typedLocation: '',
+		};
+	}
 
 	submitFormHandler = (event) => {
 		event.preventDefault();
@@ -26,6 +29,38 @@ class LocationForm extends Component {
 	suggestionClickAndClear = (placeCoord) => {
 		this.props.suggestionClickedHandler(placeCoord);
 		this.setState({ typedLocation: '' });
+		this.hideKeyboard();
+	};
+
+	hideKeyboard = () => {
+		//this set timeout needed for case when hideKeyborad
+		//is called inside of 'onfocus' event handler
+		setTimeout(function () {
+			//creating temp field
+			var field = document.createElement('input');
+			field.setAttribute('type', 'text');
+			//hiding temp field from peoples eyes
+			//-webkit-user-modify is nessesary for Android 4.x
+			field.setAttribute(
+				'style',
+				'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;'
+			);
+			document.body.appendChild(field);
+
+			//adding onfocus event handler for out temp field
+			field.onfocus = function () {
+				//this timeout of 200ms is nessasary for Android 2.3.x
+				setTimeout(function () {
+					field.setAttribute('style', 'display:none;');
+					setTimeout(function () {
+						document.body.removeChild(field);
+						document.body.focus();
+					}, 14);
+				}, 200);
+			};
+			//focusing it
+			field.focus();
+		}, 50);
 	};
 
 	render() {
