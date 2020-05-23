7798831dbb7271e461f589fc1973d95a291900f8
wojdanowski <s.wojdanowski@gmail.com>
2020-05-23 13:12:36 +0200

    air indexes norms added to sidebar cards

--- app/src/components/InputForms/EmailSubForm/EmailSubForm.js
+++ app/src/components/InputForms/EmailSubForm/EmailSubForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import * as LINKS from './../../../Utils/Links';
+import * as LINKS from '../../../Utils/LINKS';
 
 import MainButton from './../../UI/Buttons/MainButton/MainButton';
 import Aux from './../../../hoc/Auxiliary/Auxiliary';
